2f0a40489f684c538d09801ba86693931dc1fdd7
vukhaihoan <funny.khaihoan@gmail.com>
2023-04-21 15:09:22 +0700

    feat: install react-async storage

--- packages/rn-storage/src/LocalStorageHelpers.ts
+++ packages/rn-storage/src/LocalStorageHelpers.ts
@@ -1,50 +1,34 @@
+import AsyncStorage, { AsyncStorageStatic } from "@react-native-async-storage/async-storage";
 import { ShareStore } from "@tkey/common-types";
 
-import WebStorageError from "./errors";
-import { getWindow } from "./utils";
-
-const win = getWindow();
-
-function storageAvailable(type: string): boolean {
-  let storage: Storage;
+function storageAvailable(): boolean {
+  let storage: AsyncStorageStatic;
   try {
-    storage = win[type];
+    storage = AsyncStorage;
     const x = "__storage_test__";
     storage.setItem(x, x);
     storage.removeItem(x);
     return true;
   } catch (e) {
-    return (
-      e instanceof DOMException &&
-      // everything except Firefox
-      (e.code === 22 ||
-        // Firefox
-        e.code === 1014 ||
-        // test name field too, because code might not be present
-        // everything except Firefox
-        e.name === "QuotaExceededError" ||
-        // Firefox
-        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
-      // acknowledge QuotaExceededError only if there's something already stored
-      storage &&
-      storage.length !== 0
-    );
+    return false;
   }
 }
 
 export const storeShareOnLocalStorage = async (share: ShareStore, key: string): Promise<void> => {
   const fileStr = JSON.stringify(share);
-  if (!storageAvailable("localStorage")) {
-    throw WebStorageError.localStorageUnavailable();
+  if (!storageAvailable()) {
+    throw new Error("Local storage is not enabled");
   }
-  win.localStorage.setItem(key, fileStr);
+  AsyncStorage.setItem(key, fileStr);
 };
 
 export const getShareFromLocalStorage = async (key: string): Promise<ShareStore> => {
-  if (!storageAvailable("localStorage")) {
-    throw WebStorageError.localStorageUnavailable();
+  if (!storageAvailable()) {
+    throw new Error("Local storage is not enabled");
   }
-  const foundFile = win.localStorage.getItem(key);
-  if (!foundFile) throw WebStorageError.shareUnavailableInLocalStorage();
+  const foundFile = await AsyncStorage.getItem(key);
+  if (!foundFile) {
+    throw new Error("No share exists in localstorage");
+  }
   return ShareStore.fromJSON(JSON.parse(foundFile));
 };
